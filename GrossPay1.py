0b1e2aa86b9383d04b120740aa69efc054d14f6a
Sayan Mukherjee <70065597+sayan9112@users.noreply.github.com>
2020-09-21 18:35:42 +0600

    Add files via upload

---
+++ GrossPay1.py
@@ -0,0 +1,4 @@
+hrs=input("Enter Hour:")
+rate=input("Enter Rate per Hour:")
+pay=float(hrs)*float(rate)
+print("Pay:", pay)
---
+++ GrossPay2.py
@@ -0,0 +1,10 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+xx = input("Enter the Rate:")
+x = float(xx)
+if h &lt;= 40:
+
+print( h * x)
+elif h &gt; 40:
+print(40* x + (h-40)*1.5*x)
+
